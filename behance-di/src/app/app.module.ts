f23ca46d38ece5aeb6f8fcd4432d3d54a7b83a07
Ben Korody <bkorody@showingtime.com>
2018-04-18 21:43:22 -0500

    adds searchbar component, adds bootstrap4 with MD

--- behance-di/src/app/app.module.ts
+++ behance-di/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
